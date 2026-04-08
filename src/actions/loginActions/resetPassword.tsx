fa7e7effabc497cfe0d5b769b76da53961fca020
Koya-Mitomi <558koya@gmail.com>
2026-04-08 22:25:42 +0900

    リダイレクトURLの環境変数設定を追加

--- src/actions/loginActions/resetPassword.tsx
+++ src/actions/loginActions/resetPassword.tsx
@@ -33,7 +33,8 @@ export async function resetPassword(new_password: string) {
 
 const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_URL ??
     "http://localhost:3000/";
   url = url.startsWith("http") ? url : `https://${url}`;
   url = url.endsWith("/") ? url : `${url}/`;
